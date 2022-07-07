faddf5c2d4a7c2baa9f74708625346876cdc00b8
atul-7 <akohar007@gmail.com>
2022-07-07 15:53:02 +0530

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"go-bookstore/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9000", r))
+	log.Printf("The server has started at 9000\n")
+}
